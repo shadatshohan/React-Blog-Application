d2e13563e35aa25f99ae6abd74e5f8665da3f10c
shadatshohan <shadatshohan5@gmail.com>
2025-02-26 17:17:51 +0600

    React Router Implementation

---
+++ src/Missing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Missing = () => {
+  return (
+    <main><h1>Missing Page</h1></main>
+  )
+}
+
+export default Missing
---
+++ src/PostPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <main><h1>Postpage</h1></main>
+  )
+}
+
+export default PostPage
