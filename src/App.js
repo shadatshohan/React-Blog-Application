d0fd12c4c2033dcb27397972f59ef43b3e00d105
shadatshohan <shadatshohan5@gmail.com>
2025-03-27 11:57:12 +0600

    customhook_useWindows

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {useState,useEffect} from 'react';
 import {format} from 'date-fns';
 import api from './api/posts';
 import EditPost from "./EditPost";
+import useWindowSize from "./hooks/useWindowSize";
 
 function App() {
   const [posts,setPosts]=useState([]);
@@ -21,6 +22,7 @@ function App() {
   const[editTitle,setEditTitle]=useState('');
   const[editBody,setEditBody]=useState('');
   const navigate=useNavigate();
+  const {width}=useWindowSize();
 
   useEffect(()=>{
     const fetchPosts=async ()=>{
@@ -98,7 +100,7 @@ function App() {
   }
   return (
     <div className="App">
-     <Header title="React js blog"/>
+     <Header title="React js blog" width={width}/>
      <Nav search={search} setSearch={setSearch}/>
      <Routes>
      <Route exact path="/" element={<Home posts={searchResults}/>}/>
---
+++ src/hooks/useWindowSize.js
@@ -0,0 +1,25 @@
+import {useState,useEffect} from "react";
+
+const useWindowSize=()=>{
+    const [windowSize,setWindowSize]=useState({
+        width:undefined,
+        height:undefined
+    });
+useEffect(()=>{
+    const handleResize=()=>{
+        setWindowSize({
+            width:window.innerWidth,
+            height:window.innerHeight
+        });
+    }
+    handleResize();
+    window.addEventListener("resize",handleResize);
+    const cleanUp=()=>{
+        console.log('runs if a useEffect dep changes')
+        
+    }
+    return ()=>window.removeEventListener("resize",handleResize);
+},[])
+return windowSize;
+}
+export default useWindowSize;
